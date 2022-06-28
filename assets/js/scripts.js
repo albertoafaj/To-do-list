500d05bbbae6323172fa1cc652cd588b063bb457
Alberto Fernandes <albertoafaj@hotmail.com>
2022-06-28 18:51:27 -0300

    adicionados validações de formulário e estilos CSS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-const DigitarTask = document.getElementsByName('digitar_tarefas')[0];
+const DigitarTask = document.getElementById('digitar_tarefas');
 const AddTask = document.getElementsByClassName('button')[0];
 const Checkbox = document.getElementsByClassName('checkbox')[0];
 const MsgErro = document.getElementsByClassName('erro')[0];
@@ -7,6 +7,12 @@ const Check = document.getElementsByName('check');
 let idTask = 1;
 DigitarTask.addEventListener("click",digitarTask); 
 AddTask.addEventListener("click",addTask);
+
+document.addEventListener('keypress', function(e){
+    if(e.which == 13){
+        addTask();
+    }
+  }, false);
 
 function digitarTask(){
     DigitarTask.value="";
@@ -20,11 +26,12 @@ function addTask(){
         AddTask.disabled = true;
         MsgErro.style.display = 'flex';
     } else {
-        let html = '<div class="task"> <input type="checkbox" name = "check" id="t'+ idTask+'"> <label for="descTask">'+t+'</ label> </div>';
+        let html = '<div class="task"> <input type="checkbox" name = "check" id="t'+ idTask+'"> <label class="descTask">'+t+'</ label> </div>';
         Checkbox.insertAdjacentHTML("beforeend",html);
         idTask++;
-        AddTask.disabled = true;
         MsgErro.removeAttribute('style');
+        AddTask.disabled = true;
+        digitarTask();
             }
 } ;
 
